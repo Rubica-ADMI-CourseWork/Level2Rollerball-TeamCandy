7ebf22c7205b2205673cf654f09a08ae6399402e
Audrey_Akolo <akoloaudrey9@gmail.com>
2022-10-17 15:08:05 +0300

    Fixed up down camera movement.  Moving on to tweaking camera look direction when turning

--- Assets/Scripts/ForTest/PCController.cs
+++ Assets/Scripts/ForTest/PCController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PCController : MonoBehaviour
 {
     [Header("PC Movement Variables")]
-    Rigidbody rb; //the rigidbody of the PC
     [SerializeField] float pcSpeed;
-  
+    [SerializeField] float forwardTiltAdjust = 8.5f;
+    [SerializeField] float forwardTiltCutoff = 5f;
+    float zMove;
+    Rigidbody rb; //the rigidbody of the PC
 
-   
+    Vector3 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,58 @@ public class PCController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 movement = new Vector3(Input.acceleration.y, -1f, - Input.acceleration.x); //get the accelerometer values in the real world  x and y and place them against Unity's x and y
-        rb.velocity = movement * pcSpeed; //move the pc using the rigidbody by applying a velocity to it
+        ForwardBackAxis();
+        movement = new Vector3(Input.acceleration.x * 4f, Physics.gravity.y, zMove); //get the accelerometer values in the real world  x and y and place them against Unity's x and y
+         //move the pc using the rigidbody by applying a velocity to it
         
     }
 
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * pcSpeed;
+    }
+
+    void ForwardBackAxis()
+    {
+        //records whether movement is forward or back, Forward = 1, Back = -1
+        zMove = 0f;
+
+        //Get's input from the accelerometer
+        float forwardBackMovement = Input.acceleration.y;
+
+        //scale movement by a factor of 10
+        float scaledForwardBackMovement = forwardBackMovement * 10f;
+
+        //cancel out all rotation past zero(device flat on the table)
+        if (scaledForwardBackMovement > 0)
+        {
+            scaledForwardBackMovement = 0;
+        }
+
+        //only work with positive values
+        scaledForwardBackMovement = Mathf.Abs(scaledForwardBackMovement);
+
+
+        //cut of values less than tilt cutoff as these the device ergonomics at that angle
+        //are undesirable
+        if (scaledForwardBackMovement < forwardTiltCutoff)
+        {
+            scaledForwardBackMovement = 0;
+            zMove = 0f;
+        }
+        else if (scaledForwardBackMovement > forwardTiltCutoff)
+        {
+            //use the forwardTiltAdjust to determine forward and back movement
+            //forward zMove = 1; backward zMove = -1;
+            if (scaledForwardBackMovement < forwardTiltAdjust)
+            {
+                zMove = 1;
+            }
+            else
+            {
+                zMove = -1;
+            }
+        }
+    }
  
 }
---
+++ Assets/Scripts/NewThirdPersonCamera.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewThirdPersonCamera : MonoBehaviour
+{
+    [Header("Camera Movement Variables")]
+
+    //position the camera above the player
+    [SerializeField] float heightOffset;
+
+    [SerializeField] GameObject target;
+    [SerializeField] float distanceBehindTarget = 2;
+    [SerializeField] float lookSensitivity;
+    [SerializeField] float constantSensitivityModifier;
+
+    //cache of position of camera relative to the target(player)
+    Vector3 cameraOffset = Vector3.zero;
+
+    //cache of rotation input along xAxis of accelerometer
+    float xAxisInput;
+
+    private void Update()
+    {
+        //Positioning the camera at target position and add a height
+        SetPositionAndHeightOffset();
+
+        //recieve input from xAxis of accelerometer
+        xAxisInput += Input.acceleration.x * lookSensitivity * constantSensitivityModifier * Time.deltaTime;
+
+        //the ball's velocity to influnce our pitch
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //move camera behind player
+        Vector3 locationBehindplayer = new Vector3(0, 0, -distanceBehindTarget);
+
+        //create rotation to handle the accelerometer input
+        Quaternion rotation = Quaternion.Euler(0, xAxisInput, 0);
+
+        //set the position by adding the rotation around y axis and the location behind the target
+        transform.position = cameraOffset + rotation * locationBehindplayer;
+
+        //as we rotate around player, also look at player
+        transform.LookAt(cameraOffset);
+
+
+
+    }
+    private void SetPositionAndHeightOffset()
+    {
+        cameraOffset = target.transform.position;
+        cameraOffset.y += heightOffset;
+        //transform.position = cameraOffset;
+    }
+}
