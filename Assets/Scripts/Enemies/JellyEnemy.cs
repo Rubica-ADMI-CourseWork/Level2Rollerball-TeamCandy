3ddf0845cdf3a0242de6c05eee2136c691e5fde9
Audrey_Akolo <akoloaudrey9@gmail.com>
2022-10-21 16:45:40 +0300

    The jelly enemy is now chasing the pc

--- Assets/Scripts/Enemies/JellyEnemy.cs
+++ Assets/Scripts/Enemies/JellyEnemy.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
 
 public class JellyEnemy : MonoBehaviour
 {
+    [Header("Enemy Variables")]
+    NavMeshAgent pathfinder;
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Ball").transform;
+        StartCoroutine(UpdatePath());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = 0.25f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
     }
 }
