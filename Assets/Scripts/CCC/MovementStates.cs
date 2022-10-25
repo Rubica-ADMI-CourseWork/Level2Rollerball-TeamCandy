b1d12c36dd6d606507ae8c23d82fe15932356d11
Auravision <auravision.africa@gmail.com>
2022-10-25 07:11:58 +0300

    Added a ForceType enum to allow for testing in the editor of different  Rigidbody.AddForce() ForceMode parameters and their effects on slope climbing. ForceType VelocityForce seems to work the best with lower PC speed settings(because it completely discounts the ball's mass in it's calculations) We shall tweak this further during our session tomorrow 26.10.22

---
+++ Assets/Scripts/CCC/MovementStates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ForceType
+{
+    PushForce,
+    AccelerationForce,
+    ImpulseForce,
+    VelocityForce,
+    TorqueForce
+}
