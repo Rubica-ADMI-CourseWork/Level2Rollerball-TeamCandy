7c31f1dbc2351a2ad77841052d10f6dd80da93dc
Auravision <auravision.africa@gmail.com>
2022-10-25 07:30:07 +0300

    Continued testing of Force types. Noticed a bug in the movement, movement values do not reset following changes in tilt past a certain angle in the y axis. will continue working on it.

---
+++ Assets/Scripts/CCC/ForceTypes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ForceType
+{
+    AccelerationForce,
+    PushForce,
+    VelocityForce,
+    TorqueForce,
+    ImpulseForce
+}
